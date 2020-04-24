5e72839ad373ecc66f49a1d4df94fc593caf4d28
Hoang Hai Dang <hoanghaidang1998@gmail.com>
2020-04-24 23:50:40 +0700

    [Frontend] Init vue.js app

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    extends: [
+        'plugin:vue/vue3-recommended',
+    ],
+    rules: {
+        "vue/component-tags-order": ["error", {
+            "order": ["template", "script", "style"]
+        }],
+        "vue/html-indent": ["error", (4 | "tab"), {
+            "attribute": 1,
+            "baseIndent": 1,
+            "closeBracket": 0,
+            "alignAttributesVertically": true,
+            "ignores": []
+        }]
+    }
+};
