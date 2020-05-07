fadfb9267d5470897e87504011353aade0608ab0
Hoang Hai Dang <hoanghaidang1998@gmail.com>
2020-05-07 09:03:09 +0700

    [Backend] Support upload file to s3 production env

--- backend/utils/static_file_handler.py
+++ backend/utils/static_file_handler.py
@@ -3,33 +3,83 @@ import binascii
 from django.conf import settings
 from PIL import Image
 import base64
+from io import BytesIO
+import boto3
+import datetime
+
+from environment import DJANGO_ENV
+
+
+def make_thumbnail(file):
+    thumbnail_size = (200, 200)
+    image = Image.open(file)
+    backgound = Image.new('RGBA', thumbnail_size, (255, 255, 255, 0))
+    image.thumbnail(thumbnail_size, Image.ANTIALIAS)
+    backgound.paste(image,
+                    (int((thumbnail_size[0] - image.size[0]) / 2), int((thumbnail_size[1] - image.size[1]) / 2)))
+    buffered = BytesIO()
+    backgound.save(buffered, format='PNG')
+    return buffered
+
+
+def upload_local(file, path):
+    abs_dir = os.path.join(settings.UPLOAD_FILE_DIR, os.path.dirname(path))
+    abs_path = os.path.join(settings.UPLOAD_FILE_DIR, path)
+    os.makedirs(abs_dir, exist_ok=True)
+    with open(abs_path, 'wb+') as write_file:
+        write_file.write(file.getbuffer())
+
+
+def download_local(path):
+    abs_path = os.path.join(settings.UPLOAD_FILE_DIR, path)
+    with open(abs_path, 'rb+') as file:
+        return base64.b64encode(file.read())
+
+
+def upload_s3(file, path):
+    s3 = boto3.client(service_name='s3', aws_access_key_id=settings.AWS_ACCESS_KEY,
+                      aws_secret_access_key=settings.AWS_ACCESS_TOKEN)
+    bucket_name = settings.AWS_BUCKET_NAME
+    file.seek(0)
+    s3.put_object(Bucket=bucket_name, Key=path, Body=file)
+
+
+def download_s3(path):
+    s3 = boto3.client(service_name='s3', aws_access_key_id=settings.AWS_ACCESS_KEY,
+                      aws_secret_access_key=settings.AWS_ACCESS_TOKEN)
+    bucket_name = settings.AWS_BUCKET_NAME
+    tmp_file = os.path.join('/tmp', f"{str(datetime.datetime.now())}.png")
+    s3.download_file(bucket_name, path, tmp_file)
+    with open(tmp_file, 'rb+') as file:
+        return base64.b64encode(file.read())
 
 
 def file_uploader(base64_image, _type):
     encypted_name = f"{binascii.hexlify(os.urandom(16)).decode()}.png"
-    _dir = f"{settings.UPLOAD_FILE_DIR}/{_type}"
-    os.makedirs(_dir, exist_ok=True)
-    with open(f"{_dir}/{encypted_name}", 'wb+') as file:
-        for chunk in base64_image.chunks():
-            file.write(chunk)
-    if _type == 'templates':
-        image = Image.open(f"{_dir}/{encypted_name}")
-        thumbnail_size = (200, 200)
-        image.thumbnail(thumbnail_size, Image.ANTIALIAS)
-        backgound = Image.new('RGBA', thumbnail_size, (255, 255, 255, 0))
-        backgound.paste(image,
-                        (int((thumbnail_size[0] - image.size[0]) / 2), int((thumbnail_size[1] - image.size[1]) / 2)))
-        os.makedirs(f"{_dir}/thumbnails", exist_ok=True)
-        backgound.save(f"{_dir}/thumbnails/{encypted_name}")
-        return encypted_name
+    is_production = DJANGO_ENV == 'production'
+    file = base64_image.file
+    if is_production:
+        upload_s3(file=file, path=os.path.join(_type, encypted_name))
+        if _type == 'templates':
+            thumbnail = make_thumbnail(file=file)
+            upload_s3(file=thumbnail, path=os.path.join(_type, 'thumbnails', encypted_name))
+    else:
+        upload_local(file=file, path=os.path.join(_type, encypted_name))
+        if _type == 'templates':
+            thumbnail = make_thumbnail(file=file)
+            upload_local(file=thumbnail, path=os.path.join(_type, 'thumbnails', encypted_name))
+    return encypted_name
 
 
 def file_downloader(file_name, _type, image_type=0):
-    _dir = f"{settings.UPLOAD_FILE_DIR}/{_type}"
-    if image_type:
-        path = f"{_dir}/thumbnails/{file_name}"
+    is_production = DJANGO_ENV == 'production'
+    if is_production:
+        if image_type:
+            return download_s3(os.path.join(_type, file_name))
+        else:
+            return download_s3(os.path.join(_type, 'thumbnails', file_name))
     else:
-        path = f"{_dir}/{file_name}"
-    with open(path, 'rb+') as file:
-        file_content = base64.b64encode(file.read())
-        return file_content
+        if image_type:
+            return download_local(os.path.join(_type, file_name))
+        else:
+            return download_local(os.path.join(_type, 'thumbnails', file_name))
